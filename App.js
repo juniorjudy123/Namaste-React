649577b99a06727aed81c440d91cb59245185bc2
benoy_Judy <benoyjudy@gmail.com>
2023-07-07 16:04:19 +0530

    learn jsx and babel in React

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 
+const jsxHeading = <h1>Namaste React using JSX</h1>;
+console.log(jsxHeading);
+
+const root = document.getElementById("root");
+
+ReactDOM.render(jsxHeading, root);
