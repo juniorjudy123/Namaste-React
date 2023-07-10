33c7f8f250b840149f9737a9d0eb3b55e91756e1
benoy_Judy <benoyjudy@gmail.com>
2023-07-11 05:18:43 +0530

    day 4 started building an app

--- App.js
+++ App.js
@@ -1,18 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-const Title = () => (
-    <h1 className="head">Namaste React using JSX</h1>
-);
-
-const HeadingComponent = () => {
+const Header = () => {
     return (
-        <div>
-            <Title />
-            <h1 className="heading">Namaste React functional HeadingComponent</h1>
-        </div>
-    );
-};
+        <div className="header">
+            <div className="Logo-container">
+                <img src="https://dynamic.brandcrowd.com/asset/logo/7f982a19-779d-4dd3-b533-7a9f66474000/logo-search-grid-1x?logoTemplateVersion=1&v=637810055012670000"
+                    className="logo"
+                    alt="header-logo"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>About us</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                    <li>Cart</li>
+                </ul>
 
-const root = document.getElementById("root");
-ReactDOM.render(<HeadingComponent />, root);
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" >
+            <h3>Meghna foods</h3>
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <input type="text" placeholder="Search food" ></input>
+                <button type="text">click here</button>
+            </div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+
+        </div>
+
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />)
+
+
+
